05478131cbf646e591950dd625e646b7e662833c
arman-tache <arman-tache@gmail.com>
2020-01-08 13:53:53 +0530

    Add all my Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';    
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Employee';
+  username;
+  password;
+  firstView=true;
+  secondView;
+  fname;
+  lname;
+  address;
+  city;
+  state;
+  zip;
+  login()
+  {
+    if(this.username=="Tannu17" && this.password=="Haroon@123" )
+    {
+      
+      this.firstView=false;
+      this.secondView=true;
+      console.log("hello user");
+    }
+    else{
+    alert('username and password is not corect');
+    }
+  }
+  arr: any[]=[];    
+  onRegister(form : NgForm) {
+    this.arr = form.value
+    console.log('array', this.arr);
+    console.log('value', form.value);
+   }
+ 
 }
